229c3efa5acc715e88eef2800d5f38de7fc07e82
ashpil <ashpil@pm.me>
2022-11-30 20:11:59 -0500

    remove unnecessary stages from payload fields

--- shaders/primary/payload.hlsl
+++ shaders/primary/payload.hlsl
@@ -1,10 +1,10 @@
 struct [raypayload] Payload {
     bool done : read(caller) : write(closesthit, miss);
 
-    uint materialIndex : read(caller) : write(closesthit, miss);
+    uint materialIndex : read(caller) : write(closesthit);
 
-    float3 position : read(caller) : write(closesthit, miss);
-    float3 normal : read(caller) : write(closesthit, miss);
-    float3 tangent : read(caller) : write(closesthit, miss);
-    float2 texcoord : read(caller) : write(closesthit, miss);
+    float3 position : read(caller) : write(closesthit);
+    float3 normal : read(caller) : write(closesthit);
+    float3 tangent : read(caller) : write(closesthit);
+    float2 texcoord : read(caller) : write(closesthit);
 };
