89b24b7e5d0247d033e70bec823e71c289c44463
ashpil <ashpil@pm.me>
2023-02-14 18:49:27 -0500

    clean up main integrator a bit

--- shaders/primary/integrator.hlsl
+++ shaders/primary/integrator.hlsl
@@ -33,6 +33,7 @@ struct PathTracingIntegrator : Integrator {
 
         // main path tracing loop
         for (Intersection its = Intersection::find(ray); its.hit(); its = Intersection::find(ray)) {
+
             // decode mesh attributes and material from intersection
             uint instanceID = dInstances[its.instanceIndex].instanceID();
             Geometry geometry = getGeometry(instanceID, its.geometryIndex);
@@ -40,16 +41,35 @@ struct PathTracingIntegrator : Integrator {
             MaterialParameters materialParams = MaterialParameters::create(materialIdx(instanceID, its.geometryIndex), attrs.texcoord, attrs.frame);
             StandardPBR material = materialParams.getStandardPBR();
 
-            // add emissive light at point if light not explicitly sampled or initial bounce
+            float3 outgoingDirWs = -ray.Direction;
+
+            // select proper shading normal
+            Frame shadingFrame;
+            if (dot(outgoingDirWs, materialParams.frame.n) > 0) {
+                // prefer texture normal if we can
+                shadingFrame = materialParams.frame;
+            } else if (dot(outgoingDirWs, attrs.frame.n) > 0) {
+                // if texture normal not valid, try shading normal
+                shadingFrame = attrs.frame;
+            } else {
+                // otherwise fall back to triangle normal
+                shadingFrame = attrs.triangleFrame;
+            }
+
+            float3 outgoingDirSs = shadingFrame.worldToFrame(outgoingDirWs);
+
+            // collect light from emissive meshes
             if (mesh_samples_per_bounce == 0 || bounceCount == 0 || !geometry.sampled) {
+                // add emissive light at point if light not explicitly sampled or initial bounce
                 accumulatedColor += throughput * materialParams.emissive;
             } else if (geometry.sampled) {
+                // MIS emissive light if it is sampled at later bounces
                 float lightPdf;
                 {
                     float3 samplePositionToEmitterPositionWs = attrs.position - ray.Origin;
                     float r2 = dot(samplePositionToEmitterPositionWs, samplePositionToEmitterPositionWs);
                     float sum = dEmitterAliasTable[0].select;
-                    lightPdf = r2 / (abs(dot(-ray.Direction, attrs.frame.n)) * sum);
+                    lightPdf = r2 / (abs(Frame::cosTheta(outgoingDirSs)) * sum);
                 }
 
                 if (lightPdf > 0.0) {
@@ -58,27 +78,16 @@ struct PathTracingIntegrator : Integrator {
                 }
             }
 
-            Frame shadingFrame;
-            if (dot(-ray.Direction, materialParams.frame.n) > 0) {
-                shadingFrame = materialParams.frame;
-            } else if (dot(-ray.Direction, attrs.frame.n) > 0) {
-                shadingFrame = attrs.frame;
-            } else {
-                shadingFrame = attrs.triangleFrame;
-            }
-
-            float3 outgoing = shadingFrame.worldToFrame(-ray.Direction);
-
             // accumulate direct light samples from env map
             for (uint directCount = 0; directCount < env_samples_per_bounce; directCount++) {
                 float2 rand = float2(rng.getFloat(), rng.getFloat());
-                accumulatedColor += throughput * estimateDirectMISLight(shadingFrame, EnvMap::create(), material, outgoing, attrs.position, attrs.triangleFrame.n, rand, env_samples_per_bounce) / env_samples_per_bounce;
+                accumulatedColor += throughput * estimateDirectMISLight(shadingFrame, EnvMap::create(), material, outgoingDirSs, attrs.position, attrs.triangleFrame.n, rand, env_samples_per_bounce) / env_samples_per_bounce;
             }
 
             // accumulate direct light samples from emissive meshes
             for (uint directCount = 0; directCount < mesh_samples_per_bounce; directCount++) {
                 float2 rand = float2(rng.getFloat(), rng.getFloat());
-                accumulatedColor += throughput * estimateDirectMISLight(shadingFrame, MeshLights::create(), material, outgoing, attrs.position, attrs.triangleFrame.n, rand, mesh_samples_per_bounce) / mesh_samples_per_bounce;
+                accumulatedColor += throughput * estimateDirectMISLight(shadingFrame, MeshLights::create(), material, outgoingDirSs, attrs.position, attrs.triangleFrame.n, rand, mesh_samples_per_bounce) / mesh_samples_per_bounce;
             }
 
             // possibly terminate if reached max bounce cutoff or lose at russian roulette
@@ -92,23 +101,25 @@ struct PathTracingIntegrator : Integrator {
             }
             
             // sample direction for next bounce
-            MaterialSample sample = material.sample(outgoing, float2(rng.getFloat(), rng.getFloat()));
+            MaterialSample sample = material.sample(outgoingDirSs, float2(rng.getFloat(), rng.getFloat()));
             if (sample.pdf == 0.0) return accumulatedColor;
             lastMaterialPdf = sample.pdf;
 
             // set up info for next bounce
             ray.Direction = shadingFrame.frameToWorld(sample.dirFs);
             ray.Origin = offsetAlongNormal(attrs.position, faceForward(attrs.triangleFrame.n, ray.Direction));
-            throughput *= material.eval(sample.dirFs, outgoing) * abs(Frame::cosTheta(sample.dirFs)) / sample.pdf;
+            throughput *= material.eval(sample.dirFs, outgoingDirSs) * abs(Frame::cosTheta(sample.dirFs)) / sample.pdf;
             bounceCount += 1;
         }
 
         // we only get here on misses -- terminations for other reasons return from loop
 
-        // add background color if it isn't explicitly sampled or this is a primary ray
+        // handle env map
         if (env_samples_per_bounce == 0 || bounceCount == 0) {
+            // add background color if it isn't explicitly sampled or this is a primary ray
             accumulatedColor += throughput * EnvMap::create().incomingRadiance(ray.Direction);
         } else {
+            // MIS env map if it is sampled at later bounces
             float lightPdf;
             {
                 float2 phiTheta = cartesianToSpherical(ray.Direction);
