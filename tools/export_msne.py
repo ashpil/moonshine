0cbae987e658613d3f34c3a48b538947f210a8d5
ashpil <ashpil@pm.me>
2023-03-25 22:55:21 -0400

    msne export: standard pbr material

--- tools/export_msne.py
+++ tools/export_msne.py
@@ -13,6 +13,7 @@ bl_info = {
 import bpy
 from mathutils import *
 import struct
+from collections import defaultdict
 
 def write(context, filepath):
     def write_u32(file, u32):
@@ -51,7 +52,7 @@ def write(context, filepath):
                 write_f32(file, normal.vector[2])
         write_bool(file, False) # TODO: if texcoords
         
-    def write_object(file, object, meshes):
+    def write_object(file, object, meshes, materials):
         # transformation matrix
         for v in object.matrix_world[0:3]:
             for el in v:
@@ -59,7 +60,7 @@ def write(context, filepath):
         write_bool(file, True) # visibility
         write_u32(file, 1) # geometry count
         write_u32(file, meshes[object.data]) # mesh index
-        write_u32(file, 0) # material index
+        write_u32(file, materials[object.active_material]) # material index
         write_u32(file, 0) # sampled bool
 
     def write_camera(file, camera):
@@ -87,33 +88,100 @@ def write(context, filepath):
         write_f32(file, aperture)
         # focus distance
         write_f32(file, camera.data.dof.focus_distance)
+        
+    def find_materials(scene):
+        blender_materials = set([ obj.active_material for obj in context.scene.objects if obj.type == 'MESH' ])
+        material_map = dict()
+        materials = []
+        textures1 = defaultdict(None)
+        textures2 = defaultdict(None)
+        textures3 = defaultdict(None)
+        for blender_material in blender_materials:
+            penultimate = blender_material.node_tree.get_output_node('CYCLES').inputs[0].links[0].from_node
+            assert penultimate.name == "Principled BSDF", "TODO others"
+            material = {}
+            material["type"] = "Standard PBR"
+            for input in penultimate.inputs:
+                match input.name:
+                    case "Base Color":
+                        assert not input.is_linked
+                        color = tuple(input.default_value[0:3])
+                        if color not in textures3:
+                            textures3[color] = len(textures3)
+                        material["color"] = textures3[color]
+                    case "Metallic":
+                        assert not input.is_linked
+                        value = input.default_value
+                        if value not in textures1:
+                            textures1[value] = len(textures1)
+                        material["metalness"] = textures1[value]
+                    case "Roughness":
+                        assert not input.is_linked
+                        value = input.default_value
+                        if value not in textures1:
+                            textures1[value] = len(textures1)
+                        material["roughness"] = textures1[value]
+                    case "IOR":
+                        assert not input.is_linked
+                        material["ior"] = input.default_value
+                    case "Emission":
+                        assert not input.is_linked
+                        color = tuple(input.default_value[0:3])
+                        if color not in textures3:
+                            textures3[color] = len(textures3)
+                        material["emissive"] = textures3[color]
+                    case "Normal":
+                        assert not input.is_linked
+                        n = (0.5, 0.5)
+                        if n not in textures2:
+                            textures2[n] = len(textures2)
+                        material["normal"] = textures2[n]
+                    case _:
+                        pass # ignored
+            material_map[blender_material] = len(materials)
+            materials.append(material)
+        return materials, material_map, list(textures1), list(textures2), list(textures3)
 
         
     with open(filepath, 'wb') as file:
         file.write(b"MSNE")
         # MATERIALS
+        materials, materials_map, textures1, textures2, textures3 = find_materials(context.scene)
+        print(materials)
         # textures
-        write_u32(file, 2) # total texture count
-        write_u32(file, 0) # 1x1 texture count
-        write_u32(file, 1) # 2x2 texture count
-        write_f32(file, 0.5)
-        write_f32(file, 0.5)
-        write_u32(file, 1) # 3x3 texture count
-        write_f32(file, 0.0)
-        write_f32(file, 0.0)
-        write_f32(file, 0.0)
+        total_texture_count = len(textures1) + len(textures2) + len(textures3)
+        write_u32(file, total_texture_count) # total texture count
+        write_u32(file, len(textures1)) # 1xf32 texture count
+        for texture in textures1:
+            write_f32(file, texture)
+        write_u32(file, len(textures2)) # 2xf32 texture count
+        for texture in textures2:
+            write_f32(file, texture[0])
+            write_f32(file, texture[1])
+        write_u32(file, len(textures3)) # 3xf32 texture count
+        for texture in textures3:
+            write_f32(file, texture[0])
+            write_f32(file, texture[1])
+            write_f32(file, texture[2])
         write_u32(file, 0) # dds texture count
         # material variants
         write_u32(file, 0) # glass count
         write_u32(file, 0) # lambert count
         # write_u32(file, 1) # perfect mirror count
-        write_u32(file, 0) # standard pbr count
+        write_u32(file, len(materials)) # standard pbr count
+        for material in list(materials):
+            print(material)
+            write_u32(file, len(textures1) + len(textures2) + material["color"])
+            write_u32(file, material["metalness"])
+            write_u32(file, material["roughness"])
+            write_f32(file, material["ior"])
         # materials
-        write_u32(file, 1) # material count
-        write_u32(file, 0) # normal texture index
-        write_u32(file, 1) # emissive texture index
-        write_u64(file, 2) # material variant type
-        write_u64(file, 0) # material variant index
+        write_u32(file, len(materials)) # material count
+        for i, material in enumerate(materials):
+            write_u32(file, len(textures1) + material["normal"]) # normal texture index
+            write_u32(file, len(textures1) + len(textures2) + material["emissive"]) # emissive texture index
+            write_u64(file, 3) # material variant type
+            write_u64(file, i) # material variant index
 
         # MESHES
         meshes = {}
@@ -133,7 +201,7 @@ def write(context, filepath):
         objects = [ obj for obj in context.scene.objects if obj.type == 'MESH' ]
         write_u32(file, len(objects)) # instance count
         for object in objects:
-            write_object(file, object, meshes)
+            write_object(file, object, meshes, materials_map)
 
         # CAMERA
         assert len([ obj for obj in context.scene.objects if obj.type == 'CAMERA' ]) == 1, "Must have exactly one camera"
