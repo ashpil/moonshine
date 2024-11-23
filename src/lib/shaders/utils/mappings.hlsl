e6045da795bd147d04561d7ba02c77edee5edadc
ashpil <ashpil@pm.me>
2024-11-23 00:30:31 -0800

    use better squareToTriangle

--- src/lib/shaders/utils/mappings.hlsl
+++ src/lib/shaders/utils/mappings.hlsl
@@ -2,10 +2,13 @@
 
 #include "math.hlsl"
 
+// https://eheitzresearch.wordpress.com/749-2/
 float2 squareToTriangle(float2 square) {
-    float a = 1 - sqrt(1 - square.x);
-    float b = square.y * sqrt(1 - square.x);
-    return float2(a, b);
+    if (square.x < square.y) {
+        return float2(square.x / 2, square.y - (square.x / 2));
+    } else {
+        return float2(square.x - (square.y / 2), square.y / 2);
+    }
 }
 
 float2 squareToGaussian(float2 square) {
